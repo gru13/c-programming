ca7527f20d26b6bee94687573b8c261b519b2d62
gru13 <guruprasath1302@gmail.com>
2023-01-06 07:38:27 +0530

    _3.c chamged
    Signed-off-by: gru13 <guruprasath1302@gmail.com>

--- _3.c
+++ _3.c
@@ -6,12 +6,13 @@ int main(){
     printf("enter value n : ");
     scanf("%d",&n);
     printf("First %d Fibonacci number :\n ", n);
-    while (n--)
+    while (n!=0)
     {
         printf("%d ",f0);
         temp = f0+f1;
         f0 = f1;
         f1 = temp;
+        n--;
 
     }
     getch();
